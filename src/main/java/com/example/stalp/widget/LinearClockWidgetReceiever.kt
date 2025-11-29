94c0a5111e2071fef72768b9a57826f11e5a9cdc
Zoekur <zoekur@gmail.com>
2025-11-30 00:26:42 +0100

    Add files via upload

---
+++ src/main/java/com/example/stalp/widget/LinearClockWidgetReceiever.kt
@@ -0,0 +1,7 @@
+package com.example.stalp.widget
+
+import androidx.glance.appwidget.GlanceAppWidgetReceiver
+
+class LinearClockWidgetReceiver : GlanceAppWidgetReceiver() {
+    override val glanceAppWidget = LinearClockWidget
+}
