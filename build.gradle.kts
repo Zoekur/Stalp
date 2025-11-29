5b9713b3f360bbdebd1f8254dccc78211191de94
zoekur <zoekur@gmail.com>
2025-11-30 00:30:17 +0100

    TEst

---
+++ build.gradle.kts
@@ -0,0 +1,7 @@
+// Toppnivå build file
+plugins {
+    // Standard Android plugin
+    alias(libs.plugins.android.application) apply false
+    // Standard Kotlin plugin
+    alias(libs.plugins.kotlin.android) apply false
+}
